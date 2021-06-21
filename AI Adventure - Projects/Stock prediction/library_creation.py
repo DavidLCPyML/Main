44028e738afa183223c6c0dfb64cfa25f5a34a66
flubsmeny <76233269+flubsmeny@users.noreply.github.com>
2021-06-20 22:06:49 -0500

    create directory for main file

---
+++ AI Adventure - Projects/Stock prediction/library_creation.py
@@ -0,0 +1,8 @@
+import os
+# create these folders if they does not exist
+if not os.path.isdir("results"):
+    os.mkdir("results")
+if not os.path.isdir("logs"):
+    os.mkdir("logs")
+if not os.path.isdir("data"):
+    os.mkdir("data")
