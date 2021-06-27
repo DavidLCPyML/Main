b402731b259d3b852f51d13aa4356ebed25aad2a
flubsmeny <76233269+flubsmeny@users.noreply.github.com>
2021-06-27 15:15:43 -0500

    not my own code
    used as filler code, eventually used different code to build model

--- AI Adventure - Projects/Stock prediction/library_creation.py
+++
@@ -1,8 +0,0 @@
-import os
-# create these folders if they does not exist
-if not os.path.isdir("results"):
-    os.mkdir("results")
-if not os.path.isdir("logs"):
-    os.mkdir("logs")
-if not os.path.isdir("data"):
-    os.mkdir("data")
