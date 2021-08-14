621c0b0af6e2f2d12b214317b0f38b585be758ed
DavidCPyML <76233269+DavidCPyML@users.noreply.github.com>
2021-08-13 21:23:01 -0500

    Update and rename model.py to model_func.py

---
+++ AI Adventure - Projects/MNIST Prediction/Prediction using CNN/model_func.py
@@ -0,0 +1,48 @@
+from keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
+ 
+# load train and test dataset
+def load_dataset():
+  # load dataset
+  (trainX, trainY), (testX, testY) = mnist.load_data()
+
+  # reshape + normalize dataset
+  trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+  testX = testX.reshape((testX.shape[0], 28, 28, 1))
+  trainY = to_categorical(trainY)
+  testY = to_categorical(testY)
+
+  return trainX, trainY, testX, testY
+
+# scale pixels
+def prep_pixels(train, test):
+	# convert from integers to floats
+	train_norm = train.astype('float32')
+	test_norm = test.astype('float32')
+	# normalize to range 0-1
+	train_norm = train_norm / 255.0
+	test_norm = test_norm / 255.0
+	# return normalized images
+	return train_norm, test_norm
+ 
+# define cnn model
+def define_model():
+	model = Sequential()
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Flatten())
+	model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dense(10, activation='softmax'))
+	# compile model
+	opt = SGD(lr=0.01, momentum=0.9)
+	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+	return model
